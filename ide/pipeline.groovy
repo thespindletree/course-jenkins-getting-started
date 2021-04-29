26e793bb9889bdd5ba76de991e84c96be1a04638
jc816 <thespindletree@gmail.com>
2021-04-29 16:59:33 +0100

    copy pipeeline out of jenkins web ui into a pipeline.groovy file to edit in vscode

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,30 +1,26 @@
 pipeline {
     agent any
-    triggers { pollSCM('* * * * *') }
+
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
-            }            
+                // Get some code from a GitHub repository
+                git url:'https://github.com/thespindletree/jgsu-spring-petclinic.git', branch: 'main'
+            }
         }
+
         stage('Build') {
             steps {
+                // Run Maven on a Unix agent.
                 sh './mvnw clean package'
-                //sh 'false' // true
             }
-        
+
             post {
+                //If Maven was able to run the tests, even if some of the test
+                //failed, record the test results and archive the jar file.
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-                }
-                changed {
-                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                        body: "Please go to ${BUILD_URL} and verify the build", 
-                        attachLog: true, 
-                        compressLog: true, 
-                        to: "test@jenkins",
-                        recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }

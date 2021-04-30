4fc79d1e35f885d9d320f7363a54c016f873de29
jc816 <thespindletree@gmail.com>
2021-04-30 14:32:50 +0100

    Added emailing to build outcome - currently temporarity ALWAYS

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -22,6 +22,14 @@ pipeline {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
+                //}
+                //changed {
+                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
+                        body: "Please go to ${BUILD_URL} and verify the build",
+                        attachLog: true,
+                        compressLog: true,
+                        to: "test@jenkins",
+                        recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
